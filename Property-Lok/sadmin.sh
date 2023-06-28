8fe49311c671f57acd1fb5fbe656bdaefeb45b6d
Mark Charette <mark.charette@improving.com>
2023-06-28 16:34:33 -0600

    Updated msh
    Updated msh src files

---
+++ Property-Lok/sadmin.sh
@@ -0,0 +1,2 @@
+mongosh "mongodb+srv://sadmin:prop3rtyL0k@cluster0.rayqe6t.mongodb.net/propertylok" --apiVersion 1
+
---
+++ Property-Lok/superlogin.sh
@@ -0,0 +1,2 @@
+mongosh "mongodb+srv://superadmin:yNvPpDCGvvQcwfkM@cluster0.mjj6dgl.mongodb.net/plapi" --apiVersion 1
+
